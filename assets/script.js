fa3d7e8ad622b09cff1e51d9d4cc26d8c318debd
piruleib <piruleibgames@gmail.com>
2025-06-28 15:02:02 -0300

    atualização do site

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.animation-txt', {
+    duration: 2000,
+    distance: '20px',
+});  
+
+revelar.reveal('.animation-img', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+});  
